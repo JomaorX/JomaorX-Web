2be9ef889887d7ccc212b992a3116e312feffe2b
Jose Miguel <martinez97.pro@gmail.com>
2025-06-26 13:03:00 +0200

    [UPD] Updated the contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MaterialModule } from '../../material.module';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule, MaterialModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  contactForm: FormGroup;
+  name: FormControl;
+  email: FormControl;
+  message: FormControl;
 
+  constructor() {
+    this.name = new FormControl('', Validators.required);
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.message = new FormControl('', Validators.required);
+
+    this.contactForm = new FormGroup({
+      name: this.name,
+      email: this.email,
+      message: this.message,
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      const data = this.contactForm.value;
+      console.log('Formulario enviado:', data);
+      // Aquí podrías mostrar un SnackBar o redirigir al usuario
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
 }
