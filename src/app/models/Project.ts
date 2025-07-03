ee443c6e207c0b52796ada40c86eadd0551a61a9
Jose Miguel <martinez97.pro@gmail.com>
2025-07-03 20:01:13 +0200

    [UPD] Many appearance changes and updated project information

--- src/app/models/Project.ts
+++ src/app/models/Project.ts
@@ -1,10 +1,14 @@
 export interface Project {
   id: number;
+  status?: 'development' | 'live' | 'archived';
   title: string;
   subtitle: string;
   photo: string;
-  code: string | { front: string, back: string };
-  link?: string;
+  code: string | { front: string; back: string };
+  link: string;
   skills: string | string[];
-  description?: string;
+  description: string;
+  things: string[];
+  learnings: string[];
+  features: string[];
 }
