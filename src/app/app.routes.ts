b30d800592818f74300a52bf709bbcd849c45775
JomaorX <martinez97.pro@gmail.com>
2025-12-13 11:52:17 +0100

    feat: Add hover effects to project cards and implement lazy loading for application routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,30 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { ProjectDetailComponent } from './components/project-detail/project-detail.component';
-import { ConstructionComponent } from './components/construction/construction.component';
+
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about-me', component: AboutComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'projects/:id', component: ProjectDetailComponent },
-  { path: 'construction', component: ConstructionComponent },
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'about-me',
+    loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent)
+  },
+  {
+    path: 'projects',
+    loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent)
+  },
+  {
+    path: 'projects/:id',
+    loadComponent: () => import('./components/project-detail/project-detail.component').then(m => m.ProjectDetailComponent)
+  },
+  {
+    path: 'construction',
+    loadComponent: () => import('./components/construction/construction.component').then(m => m.ConstructionComponent)
+  },
   { path: '**', redirectTo: '' },
 ];
